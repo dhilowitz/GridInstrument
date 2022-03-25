9977c5a0bdfb9a289ff2635dac7c58c4eb96ebf4
Alberto Fanjul <albertofanjul@gmail.com>
2022-03-25 20:36:46 +0100

    modifications to use python3 rtmidi

--- play.py
+++ play.py
@@ -15,13 +15,13 @@ import time
 
 def note_callback(messageType, midiNote, velocity):
 	if messageType is "note_on":
-		midiout.send_message([0x90, midiNote, velocity])
+		midiout.sendMessage(rtmidi.MidiMessage.noteOn(0x90, midiNote, velocity))
 	elif messageType is "note_off":
-		midiout.send_message([0x80, midiNote, velocity])
+		midiout.sendMessage(rtmidi.MidiMessage.noteOff(0x80, midiNote))
 
 # Create a MIDI output port
-midiout = rtmidi.MidiOut()
-midiout.open_virtual_port("Grid Instrument (Virtual Port)")
+midiout = rtmidi.RtMidiOut()
+midiout.openVirtualPort("Grid Instrument (Virtual Port)")
 
 # Set up GridInstrument
 instrument = grid_instrument.GridInstrument()
