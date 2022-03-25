7bca5e8bb8c4649757ce1455c7ec6bd4c1378e1a
Alberto Fanjul <albertofanjul@gmail.com>
2022-03-25 21:32:15 +0100

    Organize examples and project info

--- examples/midi_output.py
+++ examples/midi_output.py
@@ -4,7 +4,7 @@
 # Works with all Launchpads: Mk1, Mk2, S/Mini, Pro, XL and LaunchKey
 # 
 #
-# David Hilowitz 9/24/17
+# David Hilowitz 1/15/19
 # decided.ly / davehilowitz.com
 #
 
@@ -27,4 +27,9 @@ midiout.openVirtualPort("Grid Instrument (Virtual Port)")
 instrument = grid_instrument.GridInstrument()
 instrument.intro_message = 'grid'
 instrument.note_callback = note_callback
+instrument.launchpad_pro_velocity_multiplier = 2.5
+instrument.min_velocity = 100
+instrument.max_velocity = 100
+instrument.default_velocity = 100
+
 instrument.start()
